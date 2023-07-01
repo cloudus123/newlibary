fccf2320f288b70884064135ab46cb74c1b24bb6
cloudus123 <106908082+cloudus123@users.noreply.github.com>
2023-07-01 11:20:01 +0530

    Update docker.groovy
    latest

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,9 +1,13 @@
-def build_and_run_Image (String imageName, String containerName, String dockerfilePath, String ContextPath) {
-	stage("Build Docker Image") {
-		sh "sudo docker build -t ${imageName} -f ${dockerfilePath} ${ContextPath}"
-	}
-	stage("Run container image") {
-		sh "sudo docker run -d --name ${containerName} ${imageName}"
-	}
-
+def image_build_and_run(String imageName, String containerName, String dockerfilePath, String contextPath, String port = '', String containerPort = '') {
+    stage("Build Docker Image") {
+		steps {
+        sh "sudo docker build -t ${imageName} -f ${dockerfilePath} ${contextPath}"
+		}
+    }
+    stage("Run container image") {
+		steps {
+        def publishOption = port ? "-p ${port}:${containerPort}" : ""
+        sh "sudo docker run -d ${publishOption} --name ${containerName} ${imageName}"
+		}
+    }
 }
